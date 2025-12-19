18eea8abb76c5862ae5f02ff4529d8e004d6d36a
mohammadatif786 <mohammadatif24680@gmail.com>
2025-12-19 15:24:21 +0500

    Finalized structure and service provider

--- src/StreamPackageServiceProvider.php
+++ src/StreamPackageServiceProvider.php
@@ -15,9 +15,9 @@ class StreamPackageServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Load Resources
-        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
-        $this->loadViewsFrom(__DIR__ . '/../resources/views', 'streampackage');
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'streampackage');
+        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
 
         // Publishing Assets
         if ($this->app->runningInConsole()) {
@@ -26,11 +26,11 @@ class StreamPackageServiceProvider extends ServiceProvider
             ], 'streampackage-config');
 
             $this->publishes([
-                __DIR__ . '/../database/migrations/' => database_path('migrations')
+                __DIR__ . '/database/migrations/' => database_path('migrations')
             ], 'streampackage-migrations');
 
             $this->publishes([
-                __DIR__ . '/../database/seeders/StreamSeeder.php' => database_path('seeders/StreamSeeder.php'),
+                __DIR__ . '/database/seeders/StreamSeeder.php' => database_path('seeders/StreamSeeder.php'),
             ], 'streampackage-seeders');
 
             $this->commands([
